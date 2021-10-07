9f732f95baa3f2500800e2e44db542f81928d479
yhlu0923 <eric890923@gmail.com>
2021-10-07 19:58:44 +0800

    EricLu0923-lesson10_07-attempt2

--- lesson10_07/EricLu0923/validate.sh
+++ lesson10_07/EricLu0923/validate.sh
@@ -2,36 +2,57 @@
 
 test_path="${BASH_SOURCE[0]}"
 solution_path="$(realpath .)"
-tmp_dir=$(mktemp -d -t lab0-XXXXXXXXXX)
+tmp_dir=$(mktemp -d -t lab4-XXXXXXXXXX)
 
 echo "working directory: $tmp_dir"
 cd $tmp_dir
 
 #rm -rf *
-cp $solution_path/helloworld.go .
-result=$(go run helloworld.go 2>&1) ; ret=$?
-if [ $ret -ne 0 ] ; then
-  echo "\"go run helloworld.go\" fails ; NO POINT"
+cp $solution_path/app_url.txt .
+a=$((1 + RANDOM % 10))
+b=$((1 + RANDOM % 10))
+ans=$a" + "$b" = "$((a+b))
+curl -o result.txt `cat app_url.txt`add/$a/$b
+if [ "$(echo $ans)" != "$(cat result.txt)" ] ; then
+  echo "right ans="$ans
+  echo "your ans=$(cat result.txt)"
+  echo "wrong answer ; NO POINT"
 else
-  echo "\"go run helloworld.go\" output: \"$result\""
-  if [ "hello world" != "$(echo $result)" ] ; then
-    echo "wrong answer ; NO POINT"
-  else
-    echo "GET POINT 1"
-  fi
+  echo "GET POINT 1"
 fi
 
-cp $solution_path/username.go .
-result=$(go run username.go 2>&1) ; ret=$?
-if [ $ret -ne 0 ] ; then
-  echo "\"go run username.go\" fails ; NO POINT"
+a=$((1 + RANDOM % 10))
+b=$((1 + RANDOM % 10))
+ans=$a" - "$b" = "$((a-b))
+curl -o result.txt `cat app_url.txt`sub/$a/$b
+if [ "$(echo $ans)" != "$(cat result.txt)" ] ; then
+  echo "right ans="$ans
+  echo "your ans=$(cat result.txt)"
+  echo "wrong answer ; NO POINT"
 else
-  echo "\"go run username.go\" output: \"$result\""
-  if [ "$(basename $solution_path)" != "$(echo $result)" ] ; then
-    echo "$(basename $solution_path) wrong answer ; NO POINT"
-  else
-    echo "GET POINT 1"
-  fi
+  echo "GET POINT 1"
+fi
+a=$((1 + RANDOM % 10))
+b=$((1 + RANDOM % 10))
+ans=$a" * "$b" = "$((a*b))
+curl -o result.txt `cat app_url.txt`mul/$a/$b
+if [ "$(echo "$ans")" != "$(cat result.txt)" ] ; then
+  echo "right ans=$(echo "$ans")"
+  echo "your ans=$(cat result.txt)"
+  echo "wrong answer ; NO POINT"
+else
+  echo "GET POINT 1"
+fi
+a=$((1 + RANDOM % 10))
+b=$((1 + RANDOM % 10))
+ans=$a" / "$b" = "$((a/b))", remainder = "$((a%b))
+curl -o result.txt `cat app_url.txt`div/$a/$b
+if [ "$(echo $ans)" != "$(cat result.txt)" ] ; then
+  echo "right ans="$ans
+  echo "your ans=$(cat result.txt)"
+  echo "wrong answer ; NO POINT"
+else
+  echo "GET POINT 1"
 fi
 
 echo "deleting working directory $tmp_dir"
